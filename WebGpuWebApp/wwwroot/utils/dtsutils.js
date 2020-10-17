04ebf14194f615a1d4db289f352eb7a00bcd1686
Davant Stewart <dndstewart@hotmail.com>
2020-10-17 13:48:51 -0500

    Ver 0.007

---
+++ WebGpuWebApp/wwwroot/utils/dtsutils.js
@@ -0,0 +1,37 @@
+﻿
+
+(function () {
+
+
+    window.dtsutils = {
+
+        Test1(item) {
+            alert('dtsTest');
+        },
+
+
+        Test2(item) {
+            alert('dtsTest2');
+        },
+
+
+        Test3(item) {
+            alert('dtsTest3');
+        },
+
+        setupObject(vGroupObject) {
+            alert('setupObject');
+        },
+
+        setupGroup(vGroup) {
+
+            var vObjectGroup = vGroup["objectGroup"];
+
+            alert('setupGroup called' + vObjectGroup.length);
+
+            vObjectGroup.forEach(dtsutils.setupObject);
+        }
+
+    }
+
+})();
